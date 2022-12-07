790cb8dd9dc48613cc044c5fa3597f084847e083
jie77min <jie77min@qoo10.com>
2022-12-07 16:54:31 +0900

    adfit 수정

--- assets/js/unite.js
+++ assets/js/unite.js
@@ -339,11 +339,7 @@ function getResultHTML(obj) {
 
             result[key] = mobTable;
 
-            result[key] += `<ins class="kakao_ad_area" style="display:none;"
-            data-ad-unit = "DAN-4p3r78hInkl0naVP"
-            data-ad-width = "250"
-            data-ad-height = "250"></ins>
-            <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>`;
+            result[key] += '<ins class="kakao_ad_area" data-ad-unit = "DAN-4p3r78hInkl0naVP" data-ad-width = "250"data-ad-height = "250"></ins>';
 
             result[key] += '<div class="row result-simple-tables">' + tTable + '</div>';
             result[key] += '<div class="row result-detail-tables" style="display: none;">' + gTable + iTable + eTable + '</div>';
