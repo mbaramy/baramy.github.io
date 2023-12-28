079fc432d9d17ffe24bad285648b2e2b29997361
JiMin Kim(김지민) <jie77min@qoo10.com>
2023-12-28 22:17:38 +0900

    20231228 신규환수 추가

--- assets/js/sheet-api.js
+++ assets/js/sheet-api.js
@@ -76,7 +76,7 @@ var query = function (sql, callback) {
         params = {
             key: '1YNepNSqpcV4Xeggpl7Nfh0Ty7VWUFtEybAjdqNlYjrc',
             tq: encodeURIComponent(sql),
-            range: 'Sheet1!A1:F249',
+            range: 'Sheet1!A1:F255',
             tqx: 'responseHandler:' + callback
         },
         qs = [];
@@ -150,7 +150,7 @@ var summonQuery = function (sql, callback) {
         params = {
             key: '1YNepNSqpcV4Xeggpl7Nfh0Ty7VWUFtEybAjdqNlYjrc',
             tq: encodeURIComponent(sql),
-            range: 'Sheet3!A1:G450',
+            range: 'Sheet3!A1:G458',
             tqx: 'responseHandler:' + callback
         },
         qs = [];
