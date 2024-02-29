3c2eefc502252658e7a3145b9923470685cbf1d5
Jimin <jie77min@gmail.com>
2024-02-29 21:12:59 +0900

    240229 신규환수 추가

--- assets/js/sheet-api.js
+++ assets/js/sheet-api.js
@@ -76,7 +76,7 @@ var query = function (sql, callback) {
         params = {
             key: '1YNepNSqpcV4Xeggpl7Nfh0Ty7VWUFtEybAjdqNlYjrc',
             tq: encodeURIComponent(sql),
-            range: 'Sheet1!A1:F261',
+            range: 'Sheet1!A1:F267',
             tqx: 'responseHandler:' + callback
         },
         qs = [];
@@ -150,7 +150,7 @@ var summonQuery = function (sql, callback) {
         params = {
             key: '1YNepNSqpcV4Xeggpl7Nfh0Ty7VWUFtEybAjdqNlYjrc',
             tq: encodeURIComponent(sql),
-            range: 'Sheet3!A1:G468',
+            range: 'Sheet3!A1:G477',
             tqx: 'responseHandler:' + callback
         },
         qs = [];
